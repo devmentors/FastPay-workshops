10b19a923143e77c2b272ceec135e5080349e720
Dariusz Pawlukiewicz <dpawlukiewicz@ecovadis.com>
2021-11-22 16:24:52 +0100

    Added unit tests for VO

---
+++ tests/FastPay.UnitTests/Domain/EmailTests.cs
@@ -0,0 +1,41 @@
+﻿using FastPay.Domain.Exceptions;
+using FastPay.Domain.ValueObjects;
+using Shouldly;
+using Xunit;
+
+namespace FastPay.UnitTests.Domain
+{
+    public class EmailTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Email_Should_Throw_InvalidEmailException_When_Value_Is_Null_Or_Empty(string value)
+        {
+            var exception = Record.Exception(() => new Email(value));
+            
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<InvalidEmailException>();
+        }
+        
+        [Theory]
+        [InlineData("mail@@.test.pl")]
+        [InlineData("mailtest.pl@")]
+        public void Email_Should_Throw_InvalidEmailException_When_Value_Is_Invalid_Email(string value)
+        {
+            var exception = Record.Exception(() => new Email(value));
+            
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<InvalidEmailException>();
+        }
+
+        [Fact]
+        public void Email_Should_Assign_Value_When_Is_Valid()
+        {
+            var validEmail = "valid@email.com";
+            var email = new Email(validEmail);
+            
+            email.Value.ShouldBe(validEmail);
+        }
+    }
+}
