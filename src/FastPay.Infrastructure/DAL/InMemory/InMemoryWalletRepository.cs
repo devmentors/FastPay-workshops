25fb4b18b23338848a8032e3b6a426a7b590423e
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-11-24 10:15:17 +0100

    in memory wallet repo

---
+++ src/FastPay.Infrastructure/DAL/InMemory/InMemoryWalletRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastPay.Domain.Entities;
+using FastPay.Domain.Repositories;
+
+namespace FastPay.Infrastructure.DAL.InMemory
+{
+    internal sealed class InMemoryWalletRepository : IWalletRepository
+    {
+        // Not thread safe
+        private readonly List<Wallet> _wallets = new();
+        
+        public async Task<Wallet> GetAsync(Guid id)
+        {
+            await Task.CompletedTask;
+            return _wallets.SingleOrDefault(x => x.Id == id);
+        }
+
+        public async Task AddAsync(Wallet wallet)
+        {
+            await Task.CompletedTask;
+            _wallets.Add(wallet);
+        }
+
+        public async Task UpdateAsync(Wallet wallet)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(Wallet wallet)
+        {
+            await Task.CompletedTask;
+            _wallets.Remove(wallet);
+        }
+    }
+}
