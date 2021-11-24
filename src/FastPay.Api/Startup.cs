5663f6c15836a44ed02067e8ba37870ae0deefa1
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-11-24 13:03:05 +0100

    configuration

--- src/FastPay.Api/Startup.cs
+++ src/FastPay.Api/Startup.cs
@@ -2,20 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FastPay.Infrastructure.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace FastPay.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = configuration["database:connectionString"];
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseOptions>(_configuration.GetSection("database"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
